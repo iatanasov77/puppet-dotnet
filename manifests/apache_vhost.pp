1dc8d0f1701ea0b6f17a73de426531284b06cc11
vagrant <vagrant@myprojects.lh>
2020-10-08 09:28:47 +0000

    Fix Syntax Error.

--- manifests/apache_vhost.pp
+++ manifests/apache_vhost.pp
@@ -4,30 +4,28 @@ class vs_dotnet::apache_vhost (
     Integer $reverseProxyPort   = 5000
 ) {
     apache::vhost { "${hostName}":
-        port        => '80',
+        port            => '80',
+        serveradmin     => "webmaster@${hostName}",
+        docroot         => "${documentRoot}", 
+        override        => 'all',
+        log_level       => 'debug',
         
-        serveraliases => [
+        serveraliases   => [
             "www.${hostName}",
         ],
-        serveradmin => "webmaster@${hostName}",
-
-        docroot     => "${documentRoot}", 
-        override    => 'all',
         
-        directories => [
+        directories     => [
             {
-                path            => "${documentRoot}"
+                path            => "${documentRoot}",
                 allow_override  => ['All'],
                 'Require'       => 'all granted',
             }
         ],
         
-        custom_fragment    => "
+        custom_fragment => "
             ProxyPreserveHost On
             ProxyPass / http://127.0.0.1:${reverseProxyPort/
             ProxyPassReverse / http://127.0.0.1:${reverseProxyPort/
         ",
-        
-        log_level          => 'debug',
     }
 }
